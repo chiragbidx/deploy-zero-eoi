a0741e869ee28e725b5cf32d6be43cbbe5fdb226
chiragbidx <chirag@bidx.ai>
2026-03-18 02:08:06 +0530

    Update hero for LeadSync

--- components/home/LayoutHeroSection.tsx
+++ components/home/LayoutHeroSection.tsx
@@ -22,7 +22,7 @@ export const LayoutHeroSection = () => {
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
             <h1>
               {hero.titleBefore}
-              <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
+              <span className="text-transparent px-2 bg-gradient-to-r from-[#8896ff] to-primary bg-clip-text">
                 {hero.titleHighlight}
               </span>
               {hero.titleAfter}
